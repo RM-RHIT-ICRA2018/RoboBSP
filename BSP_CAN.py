5dcf34ba187a9dd803d8ea77c71fb5c30e1d1196
MarsTechHAN <hansh-sz@hotmail.com>
2018-03-09 21:38:39 +0000

    PID is working :)

--- BSP_CAN.py
+++ BSP_CAN.py
@@ -1,15 +1,18 @@
-import socket, struct, sys, json, time, os.path, threading
+import socket, struct, sys, json, time, os.path, threading,math
+from bitstring import Bits
 import paho.mqtt.client as mqtt
 import BSP_ERROR, BSP_PID as PID
 
 version = "00A01B " + time.ctime(os.path.getctime(os.sys.argv[0]))
 
-CHASSIS_SPEED_SETTINS = {"P":0.1, "I":0.0, "D":0.0}
+CHASSIS_SPEED_SETTINS = {"P":24.0, "I":5.0, "D":1.0}
 Chassis_1_Speed = PID.PID(CHASSIS_SPEED_SETTINS["P"], CHASSIS_SPEED_SETTINS["I"], CHASSIS_SPEED_SETTINS["D"])
-Chassis_1_Speed.SetPoint=0.0
+Chassis_1_Speed.SetPoint=-500
 Chassis_1_Speed.setSampleTime(0.001)
 
-CHASSIS_TORQUE_SETTINS = {"P":0.1, "I":0.0, "D":0.0}
+CHASSIS_TORQUE_SETTINS = {"P":0.1, "I":0.1, "D":0.0002}
+
+
 Chassis_1_Torque = PID.PID(CHASSIS_TORQUE_SETTINS["P"], CHASSIS_TORQUE_SETTINS["I"], CHASSIS_TORQUE_SETTINS["D"])
 Chassis_1_Torque.SetPoint=0.0
 Chassis_1_Torque.setSampleTime(0.001)
@@ -45,36 +48,54 @@ def on_message(client, userdata, msg):
         print(BSP_ERROR.info("SocketCAN Package Send"))
 
 def CAN_RCV_LOOP():
+    Chassis_1_Angle = 0.0
     while 1:
         can_pkt = sock.recv(16)
         can_id, length, data = struct.unpack(fmt, can_pkt)
         can_id &= socket.CAN_EFF_MASK
         data = data[:length]
 
-        if can_id == 0x208:
+        if can_id == 0x204:
             torque = data[4]*256+data[5]
             if torque >= 2**15:
-                torque = -(2**16-torque)
+                torque = -2**16+torque
             speed = data[2]*256+data[3]
             if speed >= 2**15:
-                speed = -(speed-2**16)
+                speed = (speed-2**16)
 
-            Chassis_1_Speed.update(speed)
+            Chassis_Phi = (360.0)/(8191)*(data[0]*256+data[1]) - Chassis_1_Angle
+
+
+            Chassis_1_Angle = (360.0)/(8191)*(data[0]*256+data[1])
+
+            if abs(Chassis_Phi) > 100:
+                continue
+
+            Chassis_1_Speed.update(Chassis_Phi*100)
             Chassis_1_Torque.SetPoint = Chassis_1_Speed.output
 
             Chassis_1_Torque.update(torque)
             chs1_out = Chassis_1_Torque.output
 
+            if chs1_out < 0 and abs(chs1_out)>2**15:
+                chs1_out = -2**15
+
+            if chs1_out > 2**15:
+                chs1_out = 2**15-1
+
             if chs1_out < 0:
                 chs1_out = chs1_out+65536
 
-            print("Speed: %06d Torque: %06d Data: 0x%02x 0x%02x" % (Chassis_1_Speed.output, Chassis_1_Torque.output,int(chs1_out/256), int(chs1_out-int(chs1_out/256))))
-            can_pkt = struct.pack(fmt, 0x208,8,bytes([0,0,0,0,0,0,int(chs1_out/256),int(chs1_out%256)]))
-            #sock.send(can_pkt)
-            #msg_content = {"Type": "MotorFeedback","Angle" : (360.0)/(8191)*(data[0]*256+data[1]), "Speed" : speed, "Torque" : torque, "ID" : int(can_id)-0x200}
-            #print(BSP_ERROR.info(msg_content))
+
+
+
+            print("Speed: %06d Phi: %06f Torque: %06d Data: 0x%02x 0x%02x" % (Chassis_1_Speed.output, Chassis_Phi*100 ,Chassis_1_Torque.output  ,int(int(chs1_out)/256), int(int(chs1_out)%(256))))
+            can_pkt = struct.pack(fmt, 0x200,8,bytes([0,0,0,0,0,0,int(int(chs1_out)/256),int(int(chs1_out)%256)]))
+            sock.send(can_pkt)
+            msg_content = {"Type": "MotorFeedback","Angle" : (360.0)/(8191)*(data[0]*256+data[1]), "Speed" : speed, "Torque" : torque, "ID" : int(can_id)-0x200}
+            print(BSP_ERROR.info(msg_content))
             #mqtt.publish("/MOTOR/", json.dumps(msg_content))
-            
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
