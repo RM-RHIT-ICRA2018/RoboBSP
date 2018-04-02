6c5e9bae2d0815298c2b7a31259e73722f27aa24
Mingyang Cai <caim@caim-5520.rose-hulman.edu>
2018-04-01 20:45:21 -0400

    print

--- BSP_ADVANCED.py
+++ BSP_ADVANCED.py
@@ -116,7 +116,7 @@ def on_message(client, userdata, msg):
         update_PID()
         publish_real_pid()
         for i in range(PID_NUM):
-            print(str(PIDs[i].getP()))
+            print(str(PIDs[i].SetPoint))
     
     publish_config()
 
