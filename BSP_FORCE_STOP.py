b9aa10adcbf3474662895b67ae0d52bff2e4c383
Mingyang Cai <caim@caim-5520.rose-hulman.edu>
2018-05-14 00:38:45 -0400

    grammar fix

--- BSP_FORCE_STOP.py
+++ BSP_FORCE_STOP.py
@@ -2,7 +2,7 @@ import json
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
-    print(BSP_ERROR.notice("MQTT Interface Bind Success."))
+    pass
 
 def on_message(client, userdata, msg):
     pass
@@ -11,11 +11,10 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-print(BSP_ERROR.info("MQTT Interface Start Binding."))
 
 client.connect("127.0.0.1", 1883, 60)
 
 client.loop_start()
 
 while True:
-    client.publish("/FAILSAFE/", json.dumps("Type": "ForceShutDown"))
+    client.publish("/FAILSAFE/", json.dumps({"Type": "ForceShutDown"}))
